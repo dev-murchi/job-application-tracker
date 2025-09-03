ac553ea5251dd788d56670848c6f8391043601f8
Murat ÇAY <murtcay@gmail.com>
2025-09-04 01:08:10 +0300

    feat: add cookie-parser middleware to Express app
    - Import and use cookie-parser for handling cookies
    - Enable cookie parsing for authentication and session management

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 // routers
 const authRouter = require('./routes/auth');
@@ -14,6 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // routes
 app.get('/', (req, res) => {
