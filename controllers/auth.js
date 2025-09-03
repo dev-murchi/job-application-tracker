e0ab9dfdbbc8f0ec6d7ab5b131a7ac28d84f0420
Murat ÇAY <murtcay@gmail.com>
2025-09-04 00:09:53 +0300

    feat(auth): improve user registration and login, add validator
    - Add input validation and duplicate email check to registration
    - Return extended user info on registration and login
    - Select password field explicitly for login and check credentials
    - Add lastName and location fields to User model with defaults
    - Use validator package for email validation in User model
    - Add validator dependency to package.json and package-lock.json

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,9 +3,27 @@ const { StatusCodes } = require('http-status-codes');
 const { BadRequestError, UnauthenticatedError } = require('../errors');
 
 const register = async (req, res) => {
-  const user = await User.create({ ...req.body });
+  const { name, email, password } = req.body;
 
-  res.status(StatusCodes.CREATED).json({ user: { name: user.name } });
+  if (!name || !email || !password) {
+    throw new BadRequestError('please provide all values');
+  }
+  const userAlreadyExists = await User.findOne({ email });
+  if (userAlreadyExists) {
+    throw new BadRequestError('Email already in use');
+  }
+  const user = await User.create({ name, email, password });
+
+  res.status(StatusCodes.CREATED).json({
+    user: {
+      email: user.email,
+      lastName: user.lastName,
+      location: user.location,
+      name: user.name,
+    },
+
+    location: user.location,
+  });
 };
 
 const login = async (req, res) => {
@@ -15,7 +33,7 @@ const login = async (req, res) => {
     throw new BadRequestError('Please provide email and password');
   }
 
-  const user = await User.findOne({ email });
+  const user = await User.findOne({ email }).select('+password');
 
   if (!user) {
     throw new UnauthenticatedError('Invalid Credentials');
@@ -28,7 +46,17 @@ const login = async (req, res) => {
   }
 
   const token = user.createJWT();
-  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
+  res.status(StatusCodes.OK).json({
+    user: {
+      email: user.email,
+      lastName: user.lastName,
+      location: user.location,
+      name: user.name,
+    },
+
+    location: user.location,
+    token
+  });
 };
 
 module.exports = {
--- models/User.js
+++ models/User.js
@@ -1,31 +1,45 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const validator = require('validator');
 
 const UserSchema = mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Please provide name'],
     minlength: 3,
-    maxlength: 50
+    maxlength: 20
   },
   email: {
     type: String,
     required: [true, 'Please provide email'],
-    match: [
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 
-      'Please provide valid email'
-    ],
+    validate: {
+      validator: validator.isEmail,
+      message: 'Please provide a valid email',
+    },
     unique: true
   },
   password: {
     type: String,
     required: [true, 'Please provide password'],
     minlength: 6
-  }
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    maxlength: 20,
+    default: 'lastName',
+  },
+  location: {
+    type: String,
+    trim: true,
+    maxlength: 20,
+    default: 'my city',
+  },
 });
 
 UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return;
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
