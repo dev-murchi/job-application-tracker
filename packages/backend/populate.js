257f5b7cea28c5c97d8aa8654ba25d39a47ac851
Murat ÇAY <murtcay@gmail.com>
2025-09-23 18:27:49 +0000

    seed mock job data

---
+++ packages/backend/populate.js
@@ -0,0 +1,29 @@
+const fs = require('fs/promises');
+const connectDB = require('./db/connect');
+const Job = require('./models/Job');
+
+const populateJobs = async () => {
+
+  await connectDB(process.env.MONGO_URL);
+
+  const User = require('./models/User');
+  const user = await User.findOne({ email: 'test@user.com' })
+
+  const jsonJobs = JSON.parse(await fs.readFile('./mockData.json', 'utf-8'));
+  const jobs = jsonJobs.map((job) => {
+    return { ...job, createdBy: user._id };
+  });
+
+  await Job.deleteMany({ createdBy: user._id });
+  await Job.create(jobs);
+}
+
+populateJobs()
+  .then(() => {
+    console.log('Success!!!');
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  })
