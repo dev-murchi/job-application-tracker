6601266523836c7ee0d88c5917a045bd97ba5e99
Murat ÇAY <murtcay@gmail.com>
2025-10-22 23:35:33 +0300

    feat(frontend): configure Karma for CI/CD and headless testing
    - Add karma.conf.js with ChromeHeadlessCI custom launcher
    - Install Chromium and git in frontend Docker image
    - Set Chrome binary environment variables for Karma
    - Update test script with --no-watch and --no-progress flags

---
+++ packages/frontend/karma.conf.js
@@ -0,0 +1,43 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+    ],
+    client: {
+      jasmine: {
+        random: false,
+      },
+      clearContext: false,
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/job-application-tracker/frontend'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
+    },
+    reporters: ['progress', 'kjhtml'],
+    browsers: ['ChromeHeadlessCI'],
+    restartOnFileChange: true,
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+        ],
+      }
+    }
+  });
+};
